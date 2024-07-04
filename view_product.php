af9817d2b5fd16c93b379bbcd0588a1ac55301f9
Dineth Jayawickrama <dineth2226@gmail.com>
2024-07-05 00:19:54 +0530

    Update view_product.php

--- view_product.php
+++ view_product.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Products-project</title>
     <link rel="stylesheet" href="css/style.css">
-    <!--Font awsome link-->
+    <!--Font awsome.link-->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
